92e902746d3c7a89e7973b5bbf76bfdc9e052d3b
Berend Weel <b.weel@esciencecenter.nl>
2021-06-25 16:28:03 +0200

    First implementation

---
+++ cce/main.py
@@ -0,0 +1,119 @@
+import argparse
+import contextlib
+import re
+import sys
+# namedtuple with default arguments
+# <https://stackoverflow.com/a/18348004/353337>
+from dataclasses import dataclass
+from io import StringIO
+from os import error
+from pathlib import Path
+from typing import Optional, Union
+
+from pytest import Pytester
+
+
+@dataclass
+class CodeBlock:
+    code: str
+    lineno: int
+    syntax: Optional[str] = None
+    expected_output: Optional[str] = None
+    expect_exception: bool = False
+
+
+def extract_from_file(f: Union[str, bytes, Path],
+                      encoding: Optional[str] = None,
+                      *args,
+                      **kwargs):
+    with open(f, "r", encoding=encoding) as handle:
+        return extract_from_buffer(handle, *args, **kwargs)
+
+
+def extract_from_buffer(f, max_num_lines: int = 10000):
+    out = "#!/usr/bin/env python3\n"
+    previous_line = None
+    k = 1
+
+    while True:
+        line = f.readline()
+        k += 1
+        if not line:
+            # EOF
+            break
+
+        if line.lstrip()[:3] == "~~~":
+            syntax = line.strip()[3:]
+            num_leading_spaces = len(line) - len(line.lstrip())
+            lineno = k - 1
+            # read the block
+            code_block = []
+            while True:
+                line = f.readline()
+                k += 1
+                if not line:
+                    raise RuntimeError(
+                        "Hit end-of-file prematurely. Syntax error?")
+                if k > max_num_lines:
+                    raise RuntimeError(
+                        f"File too large (> {max_num_lines} lines). Set max_num_lines."
+                    )
+                # check if end of block
+                if line.lstrip()[:3] == "~~~":
+                    break
+                # Cut (at most) num_leading_spaces leading spaces
+                nls = min(num_leading_spaces, len(line) - len(line.lstrip()))
+                line = line[nls:]
+                code_block.append(line)
+
+            line = f.readline()
+            if not line:
+                raise RuntimeError(
+                    "Hit end-of-file prematurely. Syntax error?")
+
+            if re.match("\{:\s*\.language-python\}", line.lstrip()):
+                if previous_line is None:
+                    out += "".join(code_block)
+                    continue
+
+                # check for keywords
+                m = re.match("<!--pytest-codeblocks:(.*)-->",
+                             previous_line.strip())
+                if m is None:
+                    out += "".join(code_block)
+                    continue
+
+                keyword = m.group(1)
+
+                # handle special tags
+                if keyword == "skip":
+                    continue
+                else:
+                    raise RuntimeError(
+                        'Unknown pytest-codeblocks keyword "{keyword}."')
+
+        previous_line = line
+
+    return out
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Extract code  blocks from carpentries markdown.')
+    parser.add_argument('-f',
+                        '--file',
+                        help='The file to extract from',
+                        required=True)
+
+    args = parser.parse_args()
+
+    contents = extract_from_file(args.file)
+
+    try:
+        exec(contents)
+    except Exception as e:
+        print("Exception occurred while trying to run code: ", e)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/example.md
@@ -0,0 +1,61 @@
+~~~
+1 + 1
+~~~
+{:.language-python}
+Lorem ipsum
+~~~
+1 + 2 + 3
+2 + 5
+~~~
+{:.language-python}
+something with output
+~~~
+5 + 6
+~~~
+{: .language-python}
+~~~
+11
+~~~
+{: .output}
+dolor sit amet.
+~~~
+import pytest_codeblocks
+
+pytest_codeblocks.extract_from_buffer
+~~~
+{:.language-python}
+Something that should be skipped because of the language:
+~~~
+foobar
+~~~
+{:.language-ruby}
+A shell script should be exectuted:
+```sh
+echo abc
+```
+Again with an explicit skip:
+<!--pytest-codeblocks:skip-->
+~~~
+bar
+~~~
+{:.language-python}
+
+Something that contains triple fences
+~~~
+# ~~~
+import math~~~
+{:.language-python}
+~~~
+{:.language-python}
+
+Indented code blocks:
+  ~~~
+  1 + 1 == 2
+  ~~~
+{:.language-python}
+
+"Wrong" indentation:
+~~~
+3 + 4 == 7
+  ~~~
+{:.language-python}
